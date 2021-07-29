055fc983f73d80e8e8b04251f07bba2d18e43815
Tyler Ferderer <ferderer@hotmail.com>
2021-07-29 08:08:02 -0600

    Add project files.

---
+++ TheOddsApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TheOddsApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
